04ffded7d70088589a46260f53b4a7e601352dbc
Rae Gebhart <raegebhart@gmail.com>
2023-02-02 16:47:34 -0700

    Place comments inside curly braces

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Search from "../Search/Search";
 import './Header.css'
 
 const Header = () => {
   return (
     <div>
+      {
       // logo
+      }
       <h1>Rancid <br/> Tomatillos</h1>
-      // Search 
+      <Search />
     </div>
   )
 }
