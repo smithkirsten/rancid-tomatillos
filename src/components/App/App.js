b39e5a92a3eb441d30101f0488bd35d92b0b7439
Rae Gebhart <raegebhart@gmail.com>
2023-02-02 15:39:37 -0700

    Set up App structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,32 @@ import React, { Component } from 'react'
 import './App.css';
 
 class App extends Component {
-  
+  constructor() {
+    super() 
+    this.state = {
+      error: '',
+      movies: [],
+      selectedMovie: ''
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        //Header
+          //Logo
+            //Image
+          //Search Bar
+
+        //Slider
+          //worst movies
+          
+        //Slider
+          //other movies
+      </div>
+    )
+  }
+
 }
 
 export default App;
