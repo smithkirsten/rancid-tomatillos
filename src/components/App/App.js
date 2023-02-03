1c69c20e5657e799404916814277f5572eaf4303
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-03 14:02:47 -0500

    Add method to filter for worst movies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,21 +21,19 @@ class App extends Component {
     this.setState({ selectedMovie: id })
   }
 
+  findWorstMovies = () => {
+    //this currently returns any movie rated below a 4 and sorts them in order of rating
+    return this.state.movies.reduce((worst, movie) => movie.average_rating < 4 && worst.push(movie),[]).sort((a, b) => a.average_rating - b.average_rating)
+  }
+
   render() {
     return(
       <main className='App'>
         <Header />
-        {  
-        //Logo
-            //Image
-
-        //Slider
-          //worst movies
-
-        //Slider
-          //other movies
-        }
-
+        <section className="movies-display">
+          <Slider movies={} selectMovie={this.selectMovie}/>
+          <Slider movies={} selectMovie={this.selectMovie}/>
+        </section>
       </main>
     )
   }
