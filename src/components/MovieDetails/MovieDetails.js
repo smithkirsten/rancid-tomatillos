4d75c78177421f617602153354e51c3c5ee5a2dd
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-04 10:21:26 -0500

    Add method shells for MovieDetails

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,10 +1,31 @@
 import './MovieDetails.css'
 import React, { Component } from 'react'
+import MovieHeader from '../MovieHeader/MovieHeader'
+import MovieMain from '../MovieMain/MovieMain'
 
-//GET in componentdidmount
 class MovieDetails extends Component {
   constructor(props) {
-    
+    super()
+    this.state = {
+      movie: {},
+      error: ''
+    }
+  }
+  componentDidMount(props) {
+    //GET in componentdidmount
+  }
+  componentDidUpdate(props) {
+    //GET in subsequent updates to state
+  }
+
+  render(props) {
+    return (
+      <main className="App">
+        <MovieHeader />
+        <MovieMain />
+      </main>
+    )
+
   }
 }
 
