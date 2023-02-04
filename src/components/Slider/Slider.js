fcf36d33f5c485774dd2808d301223df9b97aad7
Rae Gebhart <raegebhart@gmail.com>
2023-02-04 11:17:18 -0700

    Allow movie posters to be clickable

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,25 +4,23 @@ import './Slider.css'
 import 'swiper/scss'
 import 'swiper/scss/navigation'
 import 'swiper/scss/autoplay'
-import 'swiper/scss/a11y'
-import 'swiper/css/keyboard'
 import { Autoplay, Navigation, A11y, Keyboard } from 'swiper'
 
 
 const buildABear = ({ movies, selectMovie }) => {
   console.log('# of slides: ', movies.length)
   return movies.map(movie => {
+    const aria = `Click to view details for ${movie.title}`
     return (
-    <SwiperSlide className="slide" key={movie.id}><img src={movie.poster_path} alt={movie.title} onClick={() => selectMovie(movie.id)} id={movie.id}/></SwiperSlide>
+    <SwiperSlide className="slide" key={movie.id}><button onClick={() => selectMovie(movie.id)} id={movie.id}><img aria-label={aria} src={movie.poster_path} alt={movie.title} /></button></SwiperSlide>
     )
-  })
-
+  })  
 }
 
 const Slider = (props) => {
   const slides = buildABear(props)
   const scroll = props.scroll ? { delay: 1200, disableOnInteraction: true, } : false //add autoplay if it is for all movies so they will rotate
-  const perView = props.scroll ? 7 : 7
+  // const perView = props.scroll ? 7 : 7
   const ally = {
     prevSlideMessage: "Previous slide",
     nextSlideMessage: "Next slide",
@@ -35,13 +33,13 @@ const Slider = (props) => {
       <Swiper
         spaceBetween={50}
         modules={[Navigation, Autoplay, A11y, Keyboard]}
-        slidesPerView={perView}
+        slidesPerView={7}
         navigation={true}
         direction='horizontal'
         autoplay={scroll}
         a11y={ally}
-        rewind={{enabled: true}}
-        keyboard={{enabled: true}}
+        rewind={true}
+        keyboard={true}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
         >
