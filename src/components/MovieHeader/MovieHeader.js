cbc83ebcc304f460dbbe96b7dc0f8ada40c47b4c
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-04 11:44:01 -0500

    Add props to MovieHeader

--- src/components/MovieHeader/MovieHeader.js
+++ src/components/MovieHeader/MovieHeader.js
@@ -18,19 +18,20 @@ movie: {
 
 const MovieHeader = (props) => {
   const { title, backdrop_path, rating, release_date, budget, revenue} = props.movie
-
+  //install day.js
+  const background = `background-image: url(${backdrop_path})`
   return (
-    <nav className="movie-header">
+    <nav className="movie-header" style={background}>
       <div>
-        <p className="deets movie-title"></p>
-        <p className="deets movie-rating"></p>
-        <p className="deets movie-date"></p>
+        <p className="deets movie-title">{title}</p>
+        <p className="deets movie-rating">{rating}/10</p>
+        <p className="deets movie-date">released: {release_date}</p>
       </div>
       <div>
         <button onClick={props.backToMain}>Back to All Movies</button>
         <div>
-          <p>Revenue: </p>
-          <p>Budget: </p>
+          <p>Revenue: {budget}</p>
+          <p>Budget: {revenue}</p>
         </div>
       </div>
     </nav>
