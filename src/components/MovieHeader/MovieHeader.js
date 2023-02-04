f9ca0cfa706bd72c39ebc20689cfe1d08842cf83
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-04 11:45:29 -0500

    Add export and delete notes

--- src/components/MovieHeader/MovieHeader.js
+++ src/components/MovieHeader/MovieHeader.js
@@ -1,21 +1,6 @@
 import './MovieHeader.css'
 import React, { Component } from 'react'
 
-movie: {
-  id: 1, 
-  title: "Fake Movie Title", 
-  poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
-  backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
-  release_date: "2019-12-04", 
-  overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
-  average_rating: 6, 
-  genres: ["Drama"], 
-  budget:63000000, 
-  revenue:100853753, 
-  runtime:139, 
-  tagline: "It's a movie!" 
-}
-
 const MovieHeader = (props) => {
   const { title, backdrop_path, rating, release_date, budget, revenue} = props.movie
   //install day.js
@@ -37,3 +22,5 @@ const MovieHeader = (props) => {
     </nav>
   )
 }
+
+export default MovieHeader
