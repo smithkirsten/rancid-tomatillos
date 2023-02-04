0f2064ba6a3b0b42460319f8e9c25371da240a16
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-04 18:06:36 -0500

    Create helper function to make fetch calls in both mount and update

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -14,31 +14,18 @@ class MovieDetails extends Component {
     }
   }
   //props are: movie id and backToMain function
-  componentDidMount = async(props) => {
-    console.log('movie details did mount', this.props.movieId )
-    //GET Movie
+
+  displayMovie = async(props) => {
     const movieDeets = await apiCalls.getMovies('movies', this.props.movieId)
     const videos = await apiCalls.getMovies('videos', this.props.movieId)
-    console.log('movie', movieDeets)
-    console.log('videos', videos)
     this.setState({ movie: movieDeets.movie, videos: videos })
-
-    // try {
-    //   const res = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movieId}`)
-    //   const data = await res.json() 
-    //   this.setState({movie: data.movie})
-    // } catch(err) {
-    //   console.log(err)
-    //   this.setState({ error: err })
-    // }
-    
-    //GET for individual movie details and videos
-    //movie will be returned as an object containing 1 "movie" property. Make sure to assign state to data.Movie rather than just the returned data
+  }
+  componentDidMount = async() => {
+    await this.displayMovie()
   }
 
-  componentDidUpdate(props) {
-    //GET for individual movie details and videos
-    //movie will be returned as an object containing 1 "movie" property. Make sure to assign state to data.Movie rather than just the returned data
+  componentDidUpdate = async() => {
+    await this.displayMovie()
   }
 
 
