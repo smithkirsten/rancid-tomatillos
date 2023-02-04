68032958692e73d54cf13a14fa2cb159c7bd6b47
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-04 12:11:08 -0500

    Add return for MovieMain component

--- src/components/MovieMain/MovieMain.js
+++ src/components/MovieMain/MovieMain.js
@@ -15,11 +15,23 @@ movie: {
   tagline: "It's a movie!" 
 }
 
-const MovieMain = () => {
-
+const MovieMain = (props) => {
+  const { title, tagline, runtime, genres, poster_path } = props.movie
+  const {} = props.videos
   return (
-    
+    <section className="movie-info">
+      <div className="overview-area">
+        <p className="tagline">{tagline}<span className="runtime">{runtime}</span></p>
+        <img src={poster_path} alt={title}/>
+        <p className="summary">{overview}</p>
+      </div>
+      <div className="genres">
+        <p>{genres}</p>
+      </div>
+      <Slider />
+    </section>
   )
 }
+//add videos to Slider
 
 export default MovieMain
