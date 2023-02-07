6b58a3c2e8d253977492086f1a4e47b62cf07989
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-07 18:10:56 -0500

    Fix image import

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Error.css'
 import Header from '../Header/Header'
-import '../../assets/movie.png'
+import broken from '../../assets/movie.png'
 
 const Error = ({ error }) => {
   console.log(error)
@@ -10,7 +10,7 @@ const Error = ({ error }) => {
       <Header />
       <section className="error-display">
         <h2>Whoops! We couldn't find what you're looking for</h2>
-        <img src={'../../assets/movie.png'} alt='broken movie reel' />
+        <img src={broken} alt='broken movie reel' />
       </section>
     </main>
   )
