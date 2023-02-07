15ee7544dba660ef9b5a2dadab93ae08abef742b
Rae Gebhart <raegebhart@gmail.com>
2023-02-06 17:57:49 -0700

    Remove unecessary import

--- src/components/MovieHeader/MovieHeader.js
+++ src/components/MovieHeader/MovieHeader.js
@@ -1,10 +1,7 @@
 import './MovieHeader.css'
-import dayjs from 'dayjs'
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const MovieHeader = (props) => {
-  console.log(props)
   const { title, backdrop_path, average_rating, runtime, genres } = props.movie
   const movieHours = runtime / 60
   const movieMinutes = (movieHours - Math.floor(movieHours)) * 60
@@ -17,8 +14,7 @@ const MovieHeader = (props) => {
         <p className="deets movie-runtime">{formattedTime}</p>
       </div>
       <div className='genres-back-button-section'>
-        <Link to="/main" ><button className='back-to-main-button' onClick={props.backToMain}>Back to All Movies</button>
-        </Link>
+        <button className='back-to-main-button' onClick={props.backToMain}>Back to All Movies</button>
         <div className='deets genres'>
           <p>{genres}</p>
         </div>
