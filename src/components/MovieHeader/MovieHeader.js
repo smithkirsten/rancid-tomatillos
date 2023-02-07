c6075b2866dc2b9439a87d5f6d752d47976a2aaa
Rae Gebhart <raegebhart@gmail.com>
2023-02-06 19:12:33 -0700

    Fix genre undefined bug

--- src/components/MovieHeader/MovieHeader.js
+++ src/components/MovieHeader/MovieHeader.js
@@ -7,7 +7,8 @@ const MovieHeader = (props) => {
   const movieHours = runtime / 60
   const movieMinutes = (movieHours - Math.floor(movieHours)) * 60
   const formattedTime = `${Math.floor(movieHours)} hrs ${Math.abs(Math.floor(movieMinutes))} mins`
-  const formattedGenres = genres.length > 1 ? genres.join(' | ') : genres
+  const formattedGenres = !genres ? '' : genres.length > 1 ? genres.join(' | ') : genres
+  console.log(genres)
   return (
     <nav className="movie-header" style={{ backgroundImage: `url(${backdrop_path})` }}>
       <div className='header-details'>
@@ -16,9 +17,7 @@ const MovieHeader = (props) => {
         <p className="deets movie-runtime">{formattedTime}</p>
       </div>
       <div className='genres-back-button-section'>
-        <Link to="/" >
-        <button className='back-to-main-button' onClick={props.backToMain}>Back to All Movies</button>
-        </Link>
+        <Link to="/" ><button className='back-to-main-button' onClick={props.backToMain}>Back to All Movies</button></Link>
         <div className='deets genres'>
           <p>{formattedGenres}</p>
         </div>
