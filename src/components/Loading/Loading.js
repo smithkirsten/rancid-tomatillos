3920b546bcf793fdcf3041ccb08834bafbc977b7
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-08 08:44:39 -0500

    Add loading component and loading gif

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Header from '../Header/Header'
+import Slider from '../Slider/Slider'
+
+//if loading MainPage, load dummy slides
+//if loading MovieDetails, load spinner
+const Loading = () => {
+  
+}
+
+export default Loading
