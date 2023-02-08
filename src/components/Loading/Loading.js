8bb93f657cbee2f907f98b02e983a81a9878a93f
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-08 08:51:30 -0500

    Add JSX structure for loading page

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import spinner from '../../assets/Spinner.gif'
 import Header from '../Header/Header'
 import Slider from '../Slider/Slider'
 
 //if loading MainPage, load dummy slides
 //if loading MovieDetails, load spinner
 const Loading = () => {
-  
+  return (
+    <main className='App'>
+      <Header error='loading' />
+      <section className="error-page">
+        <img src={spinner} alt='loading' className='spinner' />
+      </section>
+    </main>
+  )
 }
 
 export default Loading
