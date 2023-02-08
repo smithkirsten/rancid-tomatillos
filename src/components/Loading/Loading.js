48ddb495f023613ba0da79ea6552ae0140e6434a
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-08 09:14:34 -0500

    Add conditional rendering for all movies loading page

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -6,6 +6,19 @@ import Slider from '../Slider/Slider'
 //if loading MainPage, load dummy slides
 //if loading MovieDetails, load spinner
 const Loading = ({ allMovies }) => {
+  if (allMovies) {
+    return (
+      <main className='App'>
+        <Header error='loading' />
+        <section className='movies-display'>
+          <Slider />
+          <Slider />
+        </section>
+      </main>
+
+  
+    )
+  }
   return (
     <main className='App'>
       <Header error='loading' />
