070205781d7cac393b679c66d4fa18c39df038d6
Rae Gebhart <raegebhart@gmail.com>
2023-02-10 15:55:00 -0700

    Update youtube url

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -17,7 +17,7 @@ const buildVideoSlides = (videos) => {
       <iframe 
         width="560" 
         height="315" 
-        src={src+'?cc_load_policy=1 '} 
+        src={src} 
         title="YouTube video player" 
         frameBorder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
