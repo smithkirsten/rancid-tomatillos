b12add8c47137e2259d7dd193834d25cc829de57
Rae Gebhart <raegebhart@gmail.com>
2023-02-10 08:31:47 -0700

    Pass handleSearch down to child components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,14 @@ import './Header.css'
 import tomatillo from "../../assets/cream-toma.png"
 
 
-const Header = (props) => {
+const Header = ({ error, handleSearch }) => {
   return (
     <nav className="Header">
       <div className="logo">
         <img src={tomatillo} alt='Tomatillo Logo' className="tomatillo"/>
         <h1 className="title">Rancid <br/> Tomatillos</h1>
       </div>
-      {!props.error && <Search />}
+      {!error && <Search handleSearch={handleSearch} />}
     </nav>
   )
 }
