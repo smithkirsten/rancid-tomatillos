85fe9325f39fbf1e0ce88ce8469c4f149747a67b
Rae Gebhart <raegebhart@gmail.com>
2023-02-12 23:12:38 -0700

    Removed props keyword since it's not being used

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import './Search.css'
 
 class Search extends Component {
-  constructor(props) {
-    super(props) 
+  constructor() {
+    super() 
     this.state = {
       input: ''
     }
